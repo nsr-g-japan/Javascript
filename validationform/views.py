440af683dfaed46044ea45a997427634251c6a73
nisarbasha <nisarbasha1993@gmail.com>
2022-05-18 13:21:52 +0530

    done

--- validationform/views.py
+++ validationform/views.py
@@ -1,5 +1,17 @@
+import pyodbc as pyodbc
 from django.shortcuts import render
-
+db = pyodbc.connect('Driver={SQL server};' 'server=Nisarbasha;' 'Database=dwproject;' 'Trusted_connection=yes;')
+cursor = db.cursor()
 # Create your views here.
 def userform(request):
+    if request.method =='POST':
+        hostid = request.POST.get('hostadd')
+        username = request.POST.get('username')
+        print(hostid,username)
+        sql = ("""insert into demo values('{}','{}')"""
+               .format(hostid, username))
+        cursor.execute(sql)
+        cursor.commit()
+
+
     return render(request, 'userform.html',{})
