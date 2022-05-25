d38795b77c9e0aeb368a39994dd2fd2e9ac48990
nisarbasha <nisarbasha1993@gmail.com>
2022-05-25 16:59:48 +0530

    done

--- clientvalidation/settings.py
+++ clientvalidation/settings.py
@@ -51,7 +51,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://jsboardapp.azurewebsites.net'
+]
 
 
 ROOT_URLCONF = 'clientvalidation.urls'
